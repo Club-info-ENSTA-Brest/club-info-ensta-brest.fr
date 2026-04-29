a2078dea1b0ab86dee39292622721d51229d06c8
ArnaudLelievre48 <lolmyizi@gmail.com>
2026-04-29 08:25:07 +0200

    init commit

---
+++ backend.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask, render_template, request, url_for, redirect
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24).hex()
+
+@app.route("/")
+def home():
+    return(render_template("home.html"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
