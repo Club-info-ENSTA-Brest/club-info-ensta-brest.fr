60215af4c5390951bf56f4e052083f5825f62ff1
ArnaudLelievre48 <arnaudlelievre.fr@gmail.com>
2026-05-01 17:12:36 +0200

    changed the dots a little

--- static/script-bg.js
+++ static/script-bg.js
@@ -11,13 +11,13 @@ window.addEventListener("resize", resize);
 resize();
 
 // create dots
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 120; i++) {
   dots.push({
     x: Math.random() * canvas.width,
     y: Math.random() * canvas.height,
-    vx: (Math.random() - 0.5) * (2 * Math.random()),
-    vy: (Math.random() - 0.5) * (2 * Math.random()),
-    r: 2,
+    vx: (Math.random() - 0.5) * (1.5 * Math.random()) * (1.5 * Math.random()),
+    vy: (Math.random() - 0.5) * (1.5 * Math.random()) * (1.5 * Math.random()),
+    r: 4,
   });
 }
 
@@ -34,7 +34,14 @@ function animate() {
     if (d.y < 0) d.y = canvas.height;
     if (d.y > canvas.height) d.y = 0;
 
-    ctx.fillStyle = "rgba(230, 230, 230, 0.6)";
+    const gradient = ctx.createRadialGradient(d.x, d.y, 0, d.x, d.y, 5);
+
+    gradient.addColorStop(0.01, "rgba(220,220,220,0.9)");
+    gradient.addColorStop(0.7, "rgba(200,200,200,0.1)");
+    gradient.addColorStop(1, "rgba(0,0,0,0.0)");
+
+    ctx.fillStyle = gradient;
+
     ctx.beginPath();
     ctx.arc(d.x, d.y, d.r, 0, Math.PI * 2);
     ctx.fill();
