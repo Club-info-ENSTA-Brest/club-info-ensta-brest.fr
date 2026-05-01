87248c762984baf1b7ae557f265e74ab70d0313b
ArnaudLelievre48 <arnaudlelievre.fr@gmail.com>
2026-05-01 16:41:01 +0200

    added a slider

--- backend.py
+++ backend.py
@@ -1,12 +1,14 @@
 import os
-from flask import Flask, render_template, request, url_for, redirect
+
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.urandom(24).hex()
+app.config["SECRET_KEY"] = os.urandom(24).hex()
+
 
 @app.route("/")
 def home():
-    return(render_template("home.html"))
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
--- static/script.js
+++ static/script.js
@@ -16,7 +16,7 @@ function updateScrollState() {
 outer.addEventListener("scroll", updateScrollState);
 window.addEventListener("load", updateScrollState);
 
-// experiments
+// spotlight effect
 
 let mouseX = 0;
 let mouseY = 0;
@@ -33,5 +33,28 @@ document.addEventListener("mousemove", (e) => {
 
     el.style.setProperty("--x", `${x}px`);
     el.style.setProperty("--y", `${y}px`);
+  });
+});
+
+// selector highlight
+
+const items = document.querySelectorAll(".menu-item");
+const highlight = document.querySelector(".menu-highlight");
+const menu = document.querySelector(".pannel-menu");
+
+function moveHighlight(el) {
+  const offset = el.offsetTop;
+  highlight.style.transform = `translateY(${offset}px)`;
+}
+
+// init (Home par défaut)
+moveHighlight(items[0]);
+items[0].classList.add("active");
+
+items.forEach((item) => {
+  item.addEventListener("click", () => {
+    items.forEach((i) => i.classList.remove("active"));
+    item.classList.add("active");
+    moveHighlight(item);
   });
 });
